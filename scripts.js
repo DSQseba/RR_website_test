1da24ee23f4245e97f7e809cffa7d91421816b39
DSQseba <sebastianocavalet@gmail.com>
2024-07-02 11:55:04 +0200

    main menu overhaul

--- scripts.js
+++ scripts.js
@@ -1,11 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const banner = document.getElementById('banner');
+    /*
+    const closeBanner = document.getElementById('banner-close');*/
+    /*
+    closeBanner.addEventListener('click', function() {
+        banner.style.display = "none";
+    });
+    */
+
+    //TITLE
     const threshold = 100;
     const title = document.getElementById('title');
     const siteTitle = document.getElementById('site-title')
     const desktopMenu = document.getElementById('desktop-menu');
 
+    //MAIN MENU
+    const mainMenu = document.getElementById('main-menu');
+    const mainToggle = document.getElementById('main-toggle');
     const menuToggle = document.getElementById('menu-toggle');
+    const mainClose = document.getElementById('main-menu-close');
+    const menuShadow = document.getElementById('menu-shadow');
+
+    //ESSENTIAL MENU
+    const menuEssential = document.getElementById('essential-menu');
+    const openEssential = document.getElementById('essential');
+    const closeEssential = document.getElementById('essential-close');
+
+    //TITLE
     window.addEventListener("scroll", function() {
         if (window.scrollY > threshold) {
             title.classList.add("title-shrink");
@@ -19,36 +40,46 @@ document.addEventListener('DOMContentLoaded', () => {
             menuToggle.classList.remove("menu-toggle-shrink");
         }
     });
-    
-    const navMenu = document.getElementById('nav-menu');
-    const closeMenu = document.getElementById('close-menu');
 
-    const banner = document.getElementById('banner');
-    /*
-    const closeBanner = document.getElementById('banner-close');*/
 
-    const capiMenu = document.getElementById('capi-menu');
-    const capiToggle = document.getElementById('capi-toggle');
-    const capiClose = document.getElementById('capi-close');
-
-    /*
-    closeBanner.addEventListener('click', function() {
-        banner.style.display = "none";
-    });
-    */
-
+    //MAIN
     menuToggle.addEventListener('click', () => {
-        capiMenu.classList.toggle('active');
+        mainMenu.classList.add('active');
+        menuShadow.classList.add('active');
+        menuEssential.classList.add('menu-follow-main');
     });
 
-    capiToggle.addEventListener('click', () => {
-        capiMenu.classList.toggle('active');
+    mainToggle.addEventListener('click', () => {
+        mainMenu.classList.add('active');
+        menuShadow.classList.add('active');
+        menuEssential.classList.add('menu-follow-main');
     });
 
-    capiClose.addEventListener('click', () => {
-        capiMenu.classList.remove('active');
+    mainClose.addEventListener('click', () => {
+        mainMenu.classList.remove('active');
+        menuEssential.classList.remove('active');
+        menuShadow.classList.remove('active');
+        menuEssential.classList.remove('menu-follow-main');
     });
 
+    //ESSENTIAL
+    openEssential.addEventListener('click', () => {
+        menuEssential.classList.add('active');
+    });
+
+    closeEssential.addEventListener('click', function() {
+        menuEssential.classList.remove('active');
+    });
+
+    //CLOSE MENUS
+    menuShadow.addEventListener('click', () => {
+        mainMenu.classList.remove('active');
+        menuEssential.classList.remove('active');
+        menuShadow.classList.remove('active');
+        menuEssential.classList.remove('menu-follow-main');
+    });
+
+    //PRODUCT HOME
     const sizeButtons = document.querySelectorAll(".size-btn");
 
     sizeButtons.forEach(button => {
